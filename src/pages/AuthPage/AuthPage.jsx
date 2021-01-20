02867656cd54ab58b05fe21269ddc4366fa838f7
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-01-20 11:52:16 -0800

    complete step 3.2

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,10 @@
+import SignUpForm from '../../components/SignUpForm/SignUpForm'
 
 export default function AuthPage() {
     return(
         <main>
             <h1>AuthPage</h1>
+            <SignUpForm />
         </main>
     );
 }
