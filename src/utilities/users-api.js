96fcf4c275f430d79aafee08fbf301689bfe4ec5
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-01-20 22:48:52 -0800

    finish part 4.3

---
+++ src/utilities/users-api.js
@@ -0,0 +1,16 @@
+const BASE_URL = '/api/users';
+
+export async function signUp(userData) {
+    const res = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application.json' },
+        body: JSON.stringify(userData)
+    });
+    if (res.ok) {
+        //res.json() will resolve the JWT
+        return res.json();
+    } else {
+        throw new Error('Invalid Sign Up');
+    }
+}
+
